223c9e7929220aaf03aad0cd0cbca79cc56dccda
ostapovih <ostapovih@mydigicode.com>
2022-01-17 22:46:26 +0200

    start mvc

---
+++ src/js/app.js
@@ -0,0 +1,4 @@
+import {INIT_STRING} from "./api/service";
+
+console.log(1);
+setTimeout(() => {console.log(2); console.log(INIT_STRING)}, 5000)
--- src/js/index.js
+++
@@ -1,2 +0,0 @@
-console.log(1);
-setTimeout(() => {console.log(2)}, 5000)
