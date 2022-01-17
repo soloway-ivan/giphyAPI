6d0400a767da608a4d58d3a318c4873537d0b493
ostapovih <ostapovih@mydigicode.com>
2022-01-17 20:47:22 +0200

    delete the project

--- src/js/constants.js
+++
@@ -1,4 +0,0 @@
-const constants = {
-  API: '5Gl9B4fIpuASI0JGNgJl0hE4ceb2OyoU'
-}
-export { constants }
--- src/js/utils.js
+++
@@ -1,9 +0,0 @@
-const selectDOMElement = (selector) => {
-  return document.querySelector(selector);
-};
-
-const createDOMElement = (tag) => {
-  return document.createElement(tag);
-}
-
-export { selectDOMElement, createDOMElement }
