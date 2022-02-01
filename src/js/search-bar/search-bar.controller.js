6d0d482e152fbf8cfacd4186c96fbb57fa6b6567
Ivan <soloveii@mydigicode.com>
2022-02-01 19:14:54 +0200

    added search-bar

--- src/js/search-bar/search-bar.controller.js
+++ src/js/search-bar/search-bar.controller.js
@@ -1,13 +1,21 @@
 import {AbstractController} from "../mvc/controller";
+import { selectDomElement } from "../utils/dom-selector";
 import {SearchBarEvent} from "./search-bar.constants";
 
 export class SearchBarController extends AbstractController {
     constructor(model, view) {
         super(model, view);
-        this.registerListener(SearchBarEvent.CLICK_AWESOME_BUTTON, (data) => {this.onClickAwesomeButton(data)})
+        this.registerListener(SearchBarEvent.CLICK_SEARCHBAR_OVERLAY, (data) => { this.onClickSearchBarOverlay(data)})
+        this.registerListener(SearchBarEvent.CLICK_SEARCH, (data) => { this.onClickSearchBtn(data)})
     }
 
-    onClickAwesomeButton(data) {
-        console.log(data);
+    onClickSearchBarOverlay(DOMElement) {
+        DOMElement.classList.add('active-search-bar');
+        selectDomElement('#search-bar-overlay').style.display = 'none';
+        selectDomElement('#search-bar').classList.remove('hover-initial-search-bar');
+    }
+
+    onClickSearchBtn(data) {
+        console.log(data.value);
     }
 }
--- src/js/search-bar/search-bar.view.js
+++ src/js/search-bar/search-bar.view.js
@@ -4,26 +4,44 @@ import {selectDomElement} from "../utils/dom-selector";
 import {SearchBarEvent} from "./search-bar.constants";
 
 const template = `
-    <button id="awesome-button">click</button>
-`
+    <div class="search-bar hover-initial-search-bar" id="search-bar">
+        <input class="search-bar__input" id="search-input" type="search" placeholder="Search...">
+        <button class ="search-bar__icon" id="search-btn">
+            <img src="https://i.ibb.co/d5rcTzD/icons8-magnifying-glass-64.png" alt="search-icon" class ="search-bar__img">
+        </button>
+    <div class="search-bar__overlay" id="search-bar-overlay"></div>
+    </div>
+`;
 
 export class SearchBarView extends AbstractView {
 
-    awesomeButton;
+    searchButton;
 
     onViewInit() {
         super.onViewInit();
-        this.selector = '#search-bar';
+        this.selector = '#search-system';
         this.template = template;
     }
 
     afterViewInit() {
         super.afterViewInit();
-        this.awesomeButton = selectDomElement('#awesome-button');
-        this.awesomeButton.onclick = this.sendAwesomeEvent.bind(this);
+        this.searchBarOverlay = selectDomElement('#search-bar-overlay');
+        this.searchBtn = selectDomElement('#search-btn');
+
+        document.onkeyup = (event) => {
+            if (event.key === 'Enter') {
+                this.sendEvent(SearchBarEvent.CLICK_SEARCH);
+            };
+        };
+        this.searchBarOverlay.onclick = () => { 
+            this.sendEvent(SearchBarEvent.CLICK_SEARCHBAR_OVERLAY);
+        };
+        this.searchBtn.onclick = () => { 
+            this.sendEvent(SearchBarEvent.CLICK_SEARCH);
+        };
     }
 
-    sendAwesomeEvent() {
-        eventEmitter.emit(SearchBarEvent.CLICK_AWESOME_BUTTON, [1,2]);
+    sendEvent(event) {
+        eventEmitter.emit(event, selectDomElement('#search-input'));
     }
 }
